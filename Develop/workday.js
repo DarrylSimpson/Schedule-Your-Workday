14bd0c7b522149e4a11c4af30be452ba04f59275
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-03 14:36:45 -0500

    psuedo coded js file, and added some HTML elements for the time blocks

---
+++ Develop/workday.js
@@ -0,0 +1,8 @@
+//add current day on top of the page
+//add time blocks for standard business hours (9am-5pm)
+//color code the time blocks to show if its in the past, future or present (grey, red, green)
+//on click of the time block, you can enter an event
+//the save button functionality (text shows that was entered, and event saved within local storage)
+//when refreshed the page the saved items stay
+
+//add current day on top of the page
