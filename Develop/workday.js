569325dffb37341ce6702c1a90d0783abb05027c
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-03 22:14:10 -0500

    able to save what is typed to local storage for first row

--- Develop/workday.js
+++ Develop/workday.js
@@ -1,6 +1,3 @@
-//add time blocks for standard business hours (9am-5pm)
-//color code the time blocks to show if its in the past, future or present (grey, red, green)
-//on click of the time block, you can enter an event
 //the save button functionality (text shows that was entered, and event saved within local storage)
 //when refreshed the page the saved items stay
 
@@ -8,10 +5,13 @@ var today = moment().format('MMMM Do YYYY, h:mm a');
 var currentTime = moment().hours();
 $('#currentDay').append(today);
 
-//console.log(currentTime);
+var text = JSON.parse(localStorage.getItem('todo')) || [];
+
+
 //Based on the current time, the rows will change color to represent past, present, and future events
 $(".row").each(function () {
 
+    //if else statements to deside when a row will change color
     if($(this).attr('id') < currentTime) {
 
         $(this).find("textarea").addClass("past");
@@ -24,6 +24,28 @@ $(".row").each(function () {
 
         $(this).find("textarea").addClass("future");
     }
-
-    //console.log($(this).attr('id'))
 });
+
+function displayText(text) {
+    $('#list-item').empty();
+
+    for (var i = 0; i < text.length; i++) {
+        var textItem = $('<textarea>');
+        textItem.text(text[i]);
+    }
+};
+
+$('#save-item').on('click', function() {
+
+    var userInputToDo = $('#list-item')
+    .val()
+    .trim();
+
+    text.push(userInputToDo);
+
+    displayText(text);
+
+    localStorage.setItem('todo', JSON.stringify(text));
+});
+
+displayText(text);
