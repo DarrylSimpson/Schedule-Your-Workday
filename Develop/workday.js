269b428d81c3b10e7cdd856ddcc555f4b374aac4
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-03 21:02:58 -0500

    added color change based on time

--- Develop/workday.js
+++ Develop/workday.js
@@ -4,49 +4,26 @@
 //the save button functionality (text shows that was entered, and event saved within local storage)
 //when refreshed the page the saved items stay
 
-//add current day on top of the page
-//ADD TIME TICKING???
-//var firstRowEl = document.getElementById("9am");
-var firstRowEl = document.getElementById("9");
-var eventEl = document.getElementsByClassName("event");
-
-
 var today = moment().format('MMMM Do YYYY, h:mm a');
 var currentTime = moment().hours();
 $('#currentDay').append(today);
 
-console.log(currentTime);
-
+//console.log(currentTime);
+//Based on the current time, the rows will change color to represent past, present, and future events
 $(".row").each(function () {
 
-    if ($(this).attr('id') === currentTime){
-       
-        $('#list-item').addClass("present");
+    if($(this).attr('id') < currentTime) {
+
+        $(this).find("textarea").addClass("past");
     }
-    else {
+    else if($(this).attr('id') === currentTime) {
         
-        $('#list-item').addClass("future");
+        $(this).find("textarea").addClass("present");
+    } 
+    else if($(this).attr('id') > currentTime) {
+
+        $(this).find("textarea").addClass("future");
     }
-    console.log($(this).attr('id'))
+
+    //console.log($(this).attr('id'))
 });
-
-// function changeColor() {
-//     firstRowEl.id = '9am';
-
-//     $(".row").each(function(){
-//         console.log($(this).attr('id'))
-//     })
-    
-//     if (currentTime >= firstRowEl.id){
-//         $("list-item").addClass("present");
-        
-//     }
-//     else {
-//         $("list-item").addClass("future");
-//     };
-
-
-
-// }
-// console.log(firstRowEl);
-// changeColor();
